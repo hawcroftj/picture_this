e3585f304fc3c70d7e4c694492d0058b436c9b68
Jadon H <hawcroftj@gmail.com>
2018-03-28 16:57:54 -0500

    Implemented basic Google Maps Geocoding on Map click.

--- app/src/main/java/rrc/bit/picturethis/Map.java
+++ app/src/main/java/rrc/bit/picturethis/Map.java
@@ -1,7 +1,10 @@
 package rrc.bit.picturethis;
 
+import android.location.Address;
+import android.location.Geocoder;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -10,9 +13,16 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
 public class Map extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private Geocoder geocoder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +32,9 @@ public class Map extends FragmentActivity implements OnMapReadyCallback {
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        // initialize geocoder with default locale
+        geocoder = new Geocoder(Map.this, Locale.getDefault());
     }
 
     /**
@@ -41,5 +54,52 @@ public class Map extends FragmentActivity implements OnMapReadyCallback {
         LatLng winnipeg = new LatLng(49.895136, -97.138374);
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(winnipeg, 12));
 
+        // onMapClick will get the location of a user click
+        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng latLng) {
+                List<Address> addresses = new ArrayList<>();
+                Address address = null;
+
+                try{ // try to find an address from the selected location
+                    addresses = geocoder.getFromLocation(latLng.latitude, latLng.longitude,1);
+                } catch(IOException e){ e.printStackTrace(); }
+
+                try { // if nothing was found, display error message to user
+                    address = addresses.get(0);
+                } catch(IndexOutOfBoundsException e) {
+                    Toast.makeText(Map.this, "No data for selected location.", Toast.LENGTH_SHORT).show();
+                }
+
+                if(address != null) {
+                    String addressString = getAddressDetails(address);
+                    Toast.makeText(Map.this, addressString, Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+    }
+
+    private String getAddressDetails(Address address) {
+        StringBuilder builder = new StringBuilder();
+
+        builder.append(validateAddressData(address.getFeatureName()));
+        builder.append(validateAddressData(address.getThoroughfare()));
+        builder.append(validateAddressData(address.getLocality()));
+        builder.append(validateAddressData(address.getAdminArea()));
+        builder.append(validateAddressData(address.getCountryCode()));
+
+        return builder.toString();
+    }
+
+    /*
+     * Validate data string returned from selected LatLng location.
+     */
+    private String validateAddressData(String data) {
+        try{
+            return (data == null || data.isEmpty()) ? "--Unavailable TRY--" : data;
+        } catch(IndexOutOfBoundsException e) {
+            return "--Unavailable CATCH--";
+        }
     }
 }
